6fe795f211fc5d59ec380cba2067438d6546caab
Benjamin Brenner <ben.l.brenner@gmail.com>
2016-11-19 12:53:19 -0800

    Added geocoding for models

--- petfinder/app/models/pet.rb
+++ petfinder/app/models/pet.rb
@@ -2,5 +2,8 @@ class Pet < ApplicationRecord
 
   belongs_to :user
   attr_accessor :facebook_post_this
+  attr_accessor :address, :latitude, :longitude
+  geocoded_by :address
+  after_validation: :geocode
 
 end
--- petfinder/app/models/sighting.rb
+++ petfinder/app/models/sighting.rb
@@ -2,5 +2,8 @@ class Sighting < ApplicationRecord
 
   belongs_to :user
   attr_accessor :facebook_post_this
+  attr_accessor :address, :latitude, :longitude
+  geocoded_by :address
+  after_validation: :geocode
 
 end
