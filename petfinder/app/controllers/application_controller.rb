5b118192a563a4132d20300ccabbe3fd3e0e7cc6
Daniel Lin <david82213@hotmail.com>
2016-11-21 08:31:48 -0800

    merging

--- petfinder/app/controllers/application_controller.rb
+++ petfinder/app/controllers/application_controller.rb
@@ -1,19 +1,20 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
   # protect_from_forgery with: :null_sessions
+  @markers = []
+  
+  def authenticate_user
+    redirect_to new_session_path, alert: 'Please sign in' unless user_signed_in?
+  end
 
-    def authenticate_user
-      redirect_to new_session_path, alert: 'Please sign in' unless user_signed_in?
-    end
+  def user_signed_in?
+    session[:user_id].present?
+  end
+  helper_method :user_signed_in?
 
-    def user_signed_in?
-      session[:user_id].present?
-    end
-    helper_method :user_signed_in?
-
-    def current_user
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    end
-    helper_method :current_user
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
 
 end
